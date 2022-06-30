89581f8c62cb991aea3f70eefbf49fa4e3b48eb4
Ornelle <ornelledavilla@yahoo.fr>
2022-06-30 12:27:20 -0700

    script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-gcc -S $CFILE
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
