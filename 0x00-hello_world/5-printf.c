6136f2a3cb0a37e01a86e55037cd967bff1d5df4
Ornelle <ornelledavilla@yahoo.fr>
2022-06-30 10:28:12 -0700

    prints a line using printf instead of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - is the entry point
+ * return: always put a return function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
