8004bd981d21edc7b10903844907cda7e4a3c4ba
Ornelle <ornelledavilla@yahoo.fr>
2022-07-05 16:47:57 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- *
  * print_alphabet -> prints the lowercase alphabets
- *
  */
 
 void print_alphabet(void)
 {
 	int s;
 
-	for(s = 'a'; j <= 'z'; j++)
+	for (s = 'a'; s <= 'z'; s++)
 	{
-		_putchar(j);
+		_putchar(s);
 	}
 
 	_putchar('\n');
