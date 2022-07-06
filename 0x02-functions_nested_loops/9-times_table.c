795ccada8d902c582f2a8c29eefafe144f34d4cc
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-05 18:03:19 -0700

    correcting task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,22 +11,23 @@ void times_table(void)
 		for (y = 0; y < 10; y++)
 		{
 			k = x * y;
-			if (y == 0)
-				_putchar(k + '0');
-			if (y != 0 && k < 10)
+			if (y != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
 			}
-			else if (k >= 10)
+			if (k >= 10)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
+			else if (k < 10 && y != 0)
+			{
+				_putchar(' ');
+				_putchar((k % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
 		}
 		_putchar('\n');
 	}
