e2282b73f72b07b2f43602acc3944933abab7dcf
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-07 13:14:28 -0700

    adding 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks code for uppercase
- *@c: integer input
- *Return 1 if true and 0 otherwise
+ * _isupper - checks if a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
 
 int _isupper(int c)
