95efba57cdf659bb50ea33069ed34f6b58ea13b4
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-07 14:16:29 -0700

    adding mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - perform multiplication
+ * @a: integer variable
+ * @b: integer variable
+ * Return: 0 always
+ */
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	putchar(a * b);
+	return (0);
+}
