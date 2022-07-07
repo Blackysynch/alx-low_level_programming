6240ddfe3e9e7ab6b29542b4ef1d00136a9487ea
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-07 15:03:16 -0700

    commit task 1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print number from 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char c = 0;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar('');
+	}
+	_putchar('\n');;
+}
