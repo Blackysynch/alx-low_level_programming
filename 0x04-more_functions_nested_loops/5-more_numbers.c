d685b77051b578de5b8e8d9c91c8abba4953ddfb
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-08 09:37:17 -0700

    adding task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *more_numbers - print numbers 10 times between between 0 to 14
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		int b = 0;
+
+		for (b = 0; b <= 14; b++)
+		{
+			putchar(b + '0');
+		}
+
+		b++;
+	}
+
+	a++;
+	putchar('\n');
+}
