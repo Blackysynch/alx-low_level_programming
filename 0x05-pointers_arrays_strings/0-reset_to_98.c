afff1ffc36308d482839ca819877e63131c8f464
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-11 12:35:44 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - updates to 98
- * @n: represents the pointer
+ * reset_to_98 - reset the value in memory to 98
+ * @n: represents pointer
  * Return: Always 0.
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+	int *n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap two different integers
+ * @a: integer to be swaped
+ * @b: integer to be swaped
+ * Return: Always value
+ */
+void swap_int(int *a, int *b)
+{
+	*b = 98;
+	*a = 42;
+}
