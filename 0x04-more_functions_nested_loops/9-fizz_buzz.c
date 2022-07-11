0c5f74b1555d503807bb7e53831538211c291a24
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-10 21:26:35 -0700

    adding task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if ((c % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((c % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if (((c % 5) == 0) && ((c % 3) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		printf(" ");
+	}
+	return (0);
+}
