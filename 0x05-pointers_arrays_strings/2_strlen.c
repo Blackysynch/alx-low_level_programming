fd5261023a0280d6ab2d7242b718906206b254e3
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-12 09:04:49 -0700

    adding task 2

---
+++ 0x05-pointers_arrays_strings/2_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - gives length of a string
+ *
+ * Return: no return
+ */
+int _strlen(char *s)
+{
+	int l;
+	
+	for (l = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
