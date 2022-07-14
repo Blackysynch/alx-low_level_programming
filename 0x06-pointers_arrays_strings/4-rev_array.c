45a1d06be61a6cf8f0328c7ed16c0545c880db5c
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-14 13:16:30 -0700

    tasK 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,16 +12,13 @@ void reverse_array(int *a, int n)
 {
 	int i, j, temp;
 
-	i = 0;
-
-	j = n - 1;
-
-	while (i < j)
+	for (i = 0; i < n - 1; i++)
 	{
-		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-		i++;
-		j--;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
