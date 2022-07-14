00863dcf7a08ee622b8ef5ce553a7fe9f05cb251
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-14 10:09:01 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat- append string
+ * @dest: checked
+ * @src: checked
+ * @n: checked
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
