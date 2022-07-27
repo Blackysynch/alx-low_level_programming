706df2071ab70da51051b4ec946718ff168d12f7
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-27 12:38:07 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int = 0, j;
+
+	if (!str)
+		return (NULL);
+
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
