f4dcf1ecbb974180d721c6124943921501ba6121
Blackysynch <ornelledavilla@yahoo.fr>
2022-07-27 13:18:18 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,9 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		*(matrix + i) = (int *)malloc(width * sizeof(int));
