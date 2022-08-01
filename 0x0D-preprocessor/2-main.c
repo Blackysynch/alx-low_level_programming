494e206da553be3085b7f90d90841d28d6c131c7
Blackysynch <ornelledavilla@yahoo.fr>
2022-08-01 12:10:55 -0700

    task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 /**
  * main - check code for mame of file
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
