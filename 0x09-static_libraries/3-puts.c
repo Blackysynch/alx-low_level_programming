323dcf8b62a034f53eef480d02364efd91559f9b
Blackysynch <ornelledavilla@yahoo.fr>
2022-08-31 13:40:11 -0700

    correcting library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - print a string
+ * @str: string
+ * Return: no return value
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
