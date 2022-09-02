37471f03d4dd70cc5d5f93db7f719a0d46080fca
Blackysynch <ornelledavilla@yahoo.fr>
2022-09-02 11:38:55 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main."
+
+/**
+ * factorial - returns factorial fo number n
+ * @n : integer
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
